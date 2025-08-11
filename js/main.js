7fc9928174fb32cd65d6e35cf95b2a7e6e7fc887
karan singh <karan@karans-MacBook-Air.local>
2025-08-11 13:41:30 -0400

    Initial commit of portfolio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Wait for DOM to be fully loaded
+$(document).ready(function() {
+  // Initialize particles.js with config file
+  particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('particles.js loaded');
+  });
+
+  // Additional custom JS can go here
+});
+
